fa1d539475e9a987fb2ab9a3bab746244445daf4
lucas-devs2 <100133197+lucas-devs2@users.noreply.github.com>
2022-06-27 10:59:53 -0300

    novas views e funcionalidades adicionadas

---
+++ api2/Api2Projetofinal/storage/framework/views/bd7694a917a80091fa219d5feed6f1533a2e637b.php
@@ -0,0 +1,19 @@
+<?php
+echo("ola");
+
+/*foreach ($imovel as $imoveis) {
+echo($imoveis);
+}*/
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <a href="api@index">oi</a>
+</body>
+</html><?php /**PATH C:\Users\Connecta\Desktop\api2\resources\views/api.blade.php ENDPATH**/ ?>
